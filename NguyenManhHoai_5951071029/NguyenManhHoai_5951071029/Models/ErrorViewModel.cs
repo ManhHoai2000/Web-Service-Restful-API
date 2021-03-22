c121f9557a1f3aeebb534537a733d547188bca34
ManhHoai2000 <5951071029@st.utc2.edu.vn>
2021-03-22 18:26:32 +0700

    first commit

---
+++ NguyenManhHoai_5951071029/NguyenManhHoai_5951071029/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NguyenManhHoai_5951071029.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
